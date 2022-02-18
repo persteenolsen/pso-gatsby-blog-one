a066c686bc6ba413ab142cc021d8f94ae3913879
Per Steen Olsen <persteen22@hotmail.com>
2022-02-18 09:16:08 +0100

    Adding Posts about ASP NET and adjusting other Posts

--- src/pages/about.js
+++ src/pages/about.js
@@ -32,7 +32,7 @@ const About = () => {
                 <br/> <br/>
 				
                 <b>Last Updated</b>  <br />
-                - 17-02-2022 <br /> <br />
+                - 18-02-2022 <br /> <br />
                 				
                 <b>Main functionality</b> <br />
                                 
