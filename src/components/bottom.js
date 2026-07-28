a0447e1c0f4e272980a7116d84d34057c28ade8b
Per Olsen <persteen22@gmail.com>
2026-07-28 20:00:04 +0200

    Adjusted the bottom js

--- src/components/bottom.js
+++ src/components/bottom.js
@@ -11,7 +11,7 @@ export default function Bottom() {
        
       <div className={containerBottom}>
             
-	       ML - DL - RAG - Agentic AI  
+	       Machine Learning - Deep Learning - RAG - Agentic AI  
 
       </div>
    
